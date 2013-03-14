6ca2d9b3f3d70eb22eba73bc0a3b2cfe4f9b706b
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-03-14 11:27:34 -0500

    clear index and app.'s files

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,5 @@
 function myController($scope){
 
-$scope.myProp = 'some thing';
 
-$scope.test = function(e){
-	$scope.myProp = "Updated!!!";
-}
-
-$scope.add = function(){
-	$scope.items.push({name : $scope.myProp, fruit: $scope.selectedFruit});
-}
-
-$scope.dropDownItems = ['apple', 'banana', 'plum', 'cherry'];
-
-$scope.items = [];
 
 }
